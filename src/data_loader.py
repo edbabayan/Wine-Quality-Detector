f98ffc42db6e57f5bf772cdada25dc254c99c2e3
eduard <eduard.babayan@cognaize.com>
2024-12-21 17:13:52 +0400

    ADD: dataset loading functions

---
+++ src/data_loader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def load_data():
+    URL = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    try:
+        df = pd.read_csv(URL, sep=";")
+        return df
+    except Exception as e:
+        raise e
